f3c1694bd45e4dd536337bbb528e54eb49e70fb3
Your Name <you@example.com>
2024-06-15 11:47:30 -0500

    abraham_prescribers_sql

--- scripts/abraham_prescribers.sql
+++ scripts/abraham_prescribers.sql
@@ -88,47 +88,88 @@
 -- using (drug_name)
 -- group by neither
 -- order by drug_total_cost;
+
 -- 5. 
 --     a. How many CBSAs are in Tennessee? **Warning:** The cbsa table contains information for all states, not just Tennessee.
--- select count(cbsa)
--- from cbsa
--- 	join fipscounty
--- 	where cbsaname like '%TN%'
--- 	group by cbsaname
---56 Need help displaying total count of numbers
+-- select count(fips_county.state) as tn_cbsas
+-- from fips_county
+-- join cbsa
+-- using (fipscounty)
+-- where fips_county.state like '%TN%'
+--42 
+
 --     b. Which cbsa has the largest combined population? Which has the smallest? Report the CBSA name and total population.
--- select max(population) as max_population, cbsa.cbsaname
+-- select cbsa.cbsaname, sum(population) as total_population
 -- from population
 -- join cbsa
 -- using (fipscounty)
--- group by cbsa.cbsaname
--- order by max_population desc
--- 937847, Memphis
--- 	-------union?
--- select min(population) as min_population, cbsa.cbsaname
--- from population
--- join cbsa
--- using (fipscounty)
--- group by cbsa.cbsaname
--- order by min_population;
--- 8773 Nashville-Davidson--Murfreesboro--Franklin, TN
+-- group by 1
+-- order by total_population desc
+-- Nashville, 1830410
+-- Morristown, 116352
+ 
 --     c. What is the largest (in terms of population) county which is not included in a CBSA? Report the county name and population.
-select fipscounty
-from population
-	except
-select fipscounty
-from cbsa;
+-- select
+-- 	fips_county.county,
+-- 	max(population.population) as max_population
+-- from population
+-- join fips_county
+-- using (fipscounty)
+-- where fips_county.fipscounty not in (select fipscounty from cbsa)
+-- group by fips_county.county
+-- order by max_population desc;
+--Sevier, 95523
+
 -- 6. 
 --     a. Find all rows in the prescription table where total_claims is at least 3000. Report the drug_name and the total_claim_count.
-
+-- select drug_name, total_claim_count
+-- from prescription
+-- where total_claim_count >= 3000;
+-- Oxycodone, 4538
 --     b. For each instance that you found in part a, add a column that indicates whether the drug is an opioid.
-
+-- select drug_name, total_claim_count, drug.opioid_drug_flag
+-- from prescription
+-- join drug
+-- using (drug_name)
+-- where total_claim_count >= 3000 
+-- 	and drug.opioid_drug_flag = 'Y'
 --     c. Add another column to you answer from the previous part which gives the prescriber first and last name associated with each row.
-
+-- select drug.drug_name, prescription.total_claim_count, drug.opioid_drug_flag, prescriber.nppes_provider_first_name, prescriber.nppes_provider_last_org_name
+-- from prescription
+-- join drug
+-- on drug.drug_name= prescription.drug_name
+-- join prescriber 
+-- on prescriber.npi=prescription.npi
+-- where total_claim_count >= 3000 
+-- 	and drug.opioid_drug_flag = 'Y'
 -- 7. The goal of this exercise is to generate a full list of all pain management specialists in Nashville and the number of claims they had for each opioid. **Hint:** The results from all 3 parts will have 637 rows.
 
 --     a. First, create a list of all npi/drug_name combinations for pain management specialists (specialty_description = 'Pain Management) in the city of Nashville (nppes_provider_city = 'NASHVILLE'), where the drug is an opioid (opiod_drug_flag = 'Y'). **Warning:** Double-check your query before running it. You will only need to use the prescriber and drug tables since you don't need the claims numbers yet.
+-- select prescriber.npi, drug.drug_name
+-- from prescriber
+-- cross join drug
+-- where prescriber.nppes_provider_city = 'NASHVILLE'
+-- and opioid_drug_flag = 'Y'
+-- and prescriber.specialty_description = 'Pain Management'
 
 --     b. Next, report the number of claims per drug per prescriber. Be sure to include all combinations, whether or not the prescriber had any claims. You should report the npi, the drug name, and the number of claims (total_claim_count).
-    
+-- select total_claim_count, prescriber.npi, drug.drug_name
+-- from prescriber
+-- cross join drug
+-- left join prescription
+-- using (drug_name, npi)
+-- where prescriber.nppes_provider_city = 'NASHVILLE'
+-- and opioid_drug_flag = 'Y'
+-- and prescriber.specialty_description = 'Pain Management'
+-- order by 1 desc;
+
 --     c. Finally, if you have not done so already, fill in any missing values for total_claim_count with 0. Hint - Google the COALESCE function.
+-- select coalesce(total_claim_count, 0), prescriber.npi, drug.drug_name
+-- from prescriber
+-- cross join drug
+-- left join prescription
+-- using (drug_name, npi)
+-- where prescriber.nppes_provider_city = 'NASHVILLE'
+-- and opioid_drug_flag = 'Y'
+-- and prescriber.specialty_description = 'Pain Management'
+-- order by 1;
